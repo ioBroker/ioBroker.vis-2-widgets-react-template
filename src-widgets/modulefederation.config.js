712beb1315102ad98b605a842f344acd8918b019
GermanBluefox <dogafox@gmail.com>
2022-08-04 10:57:55 +0200

    Change translations

--- src-widgets/modulefederation.config.js
+++ src-widgets/modulefederation.config.js
@@ -4,5 +4,6 @@ module.exports = makeFederation(
     'vis2demoWidget', // internal name of package - must be unique and identical with io-package.json=>common.visWidgets.vis2demoWidget
     {
         './DemoWidget': './src/DemoWidget', // List of all widgets in this package
+        './translations': './src/translations',
     },
 );
