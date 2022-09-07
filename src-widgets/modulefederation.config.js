85b430309db8326be6ca67737528fb358f631fb8
GermanBluefox <dogafox@gmail.com>
2022-09-07 10:27:11 +0200

    Make it works

--- src-widgets/modulefederation.config.js
+++ src-widgets/modulefederation.config.js
@@ -1,7 +1,7 @@
 const makeFederation = require('@iobroker/vis-2-widgets-react-dev/modulefederation.config');
 
 module.exports = makeFederation(
-    'vis2demoWidget', // internal name of package - must be unique and identical with io-package.json=>common.visWidgets.vis2demoWidget
+    'vis2DemoWidget', // internal name of package - must be unique and identical with io-package.json=>common.visWidgets.vis2demoWidget
     {
         './DemoWidget': './src/DemoWidget', // List of all widgets in this package
         './translations': './src/translations',
--- src-widgets/src/DemoWidget.jsx
+++ src-widgets/src/DemoWidget.jsx
@@ -4,35 +4,77 @@ import {
 } from '@mui/material';
 
 import { i18n as I18n } from '@iobroker/adapter-react-v5';
-import VisRxWidget from '@iobroker/vis-2-widgets-react-dev/visRxWidget';
+import { VisRxWidget } from '@iobroker/vis-2-widgets-react-dev';
 
 class DemoWidget extends (window.visRxWidget || VisRxWidget) {
     static getWidgetInfo() {
         return {
             id: 'tplDemoWidget',
             visSet: 'demo',
-            visName: 'DemoWidget',
+            visSetLabel: 'vis_2_widgets_template', // Widget set translated label (should be defined only in one widget of set)
+            visSetColor: '#cf00ff',                // Color of widget set. it is enough to set color only in one widget of set
+            visName: 'DemoWidget',                 // Name of widget
             visAttrs: [
                 {
                     name: 'common', // group name
                     fields: [
                         {
-                            name: 'type',
+                            name: 'type',    // name in data structure
+                            label: 'vis_2_widgets_template_type', // translated field label
                             type: 'select',
                             options: ['all', 'current', 'days'],
                             default: 'all',
                         },
                     ],
                 },
-                // check here all possible types https://github.com/ioBroker/ioBroker.vis/blob/react/src/src/Attributes/Widget/WidgetField.jsx#L287
+                {
+                    name: 'private', // group name
+                    label: 'vis_2_widgets_template_private', // translated group label
+                    fields: [
+                        {
+                            name: 'oid',     // name in data structure
+                            type: 'id',
+                            label: 'vis_2_widgets_template_oid', // translated field label
+                        }
+                    ],
+                },
+                // check here all possible types https://github.com/ioBroker/ioBroker.vis/blob/react/src/src/Attributes/Widget/SCHEMA.md
             ],
             visPrev: 'widgets/vis-2-widgets-react-template/img/vis-widget-demo.png',
         };
     }
 
+    propertiesUpdate() {
+        // Widget has 3 important states
+        // 1. this.state.values - contains all state values, that are used in widget (automatically collected from widget info).
+        //                        So you can use `this.state.values[this.state.rxData.oid + '.val']` to get value of state with id this.state.rxData.oid
+        // 2. this.state.rxData - contains all widget data with replaced bindings. E.g. if this.state.data.type is `{system.adapter.admin.0.alive}`,
+        //                        then this.state.rxData.type will have state value of `system.adapter.admin.0.alive`
+        // 3. this.state.rxStyle - contains all widget styles with replaced bindings. E.g. if this.state.styles.width is `{javascript.0.width}px`,
+        //                        then this.state.rxData.type will have state value of `javascript.0.width` + 'px
+    }
+
+    componentDidMount() {
+        super.componentDidMount();
+
+        // Update data
+        this.propertiesUpdate();
+    }
+
+    // Do not delete this method. It is used by vis to read the widget configuration.
     // eslint-disable-next-line class-methods-use-this
     getWidgetInfo() {
         return DemoWidget.getWidgetInfo();
+    }
+
+    // This function is called every time when rxData is changed
+    onRxDataChanged() {
+        this.propertiesUpdate();
+    }
+
+    // This function is called every time when rxStyle is changed
+    onRxStyleChanged() {
+
     }
 
     renderWidgetBody(props) {
@@ -40,7 +82,8 @@ class DemoWidget extends (window.visRxWidget || VisRxWidget) {
 
         return <Card style={{ width: '100%', height: '100%' }}>
             <CardContent>
-                {I18n.t('My Demo widget')}
+                {I18n.t('My Demo widget: ')}
+                {this.state.values[this.state.rxData.oid + '.val']}
             </CardContent>
         </Card>;
     }
