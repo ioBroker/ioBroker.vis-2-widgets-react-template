d477b0a7631b53fe240d3c60b06e858d19014aea
oweitman <oweitman@gmx.de>
2025-06-03 17:04:35 +0200

    add javascript + vite option

--- tasks.js
+++ tasks.js
@@ -110,3 +110,33 @@ if (process.argv.includes('--typescript') || process.argv.length === 2) {
         .then(() => tsCopyAllFiles())
         .catch(e => console.error(`Cannot build: ${e}`));
 }
+
+// ------------------- tasks for javascript-vite ----------------------
+
+const SRC_JSV = 'src-widgets-jsvite/';
+const src_jsv = `${__dirname}/${SRC_JSV}`;
+
+function jsvClean() {
+    deleteFoldersRecursive(`${src_ts}build`);
+    deleteFoldersRecursive(`${__dirname}/widgets`);
+}
+
+function jsvCopyAllFiles() {
+    copyFiles([`${SRC_TSV}build/customWidgets.js`], `widgets/${adapterName}`);
+    copyFiles([`${SRC_TSV}build/assets/*.*`], `widgets/${adapterName}/assets`);
+    copyFiles([`${SRC_TSV}build/img/*`], `widgets/${adapterName}/img`);
+}
+
+if (process.argv.includes('--javascript-vite') || process.argv.length === 2) {
+    jsvClean();
+    let npmPromise;
+    if (existsSync(`${src_jsv}/node_modules`)) {
+        npmPromise = Promise.resolve();
+    } else {
+        npmPromise = npmInstall(src_jsv);
+    }
+    npmPromise
+        .then(() => buildReact(src_jsv, { rootDir: __dirname, vite: true }))
+        .then(() => jsvCopyAllFiles())
+        .catch(e => console.error(`Cannot build: ${e}`));
+}
