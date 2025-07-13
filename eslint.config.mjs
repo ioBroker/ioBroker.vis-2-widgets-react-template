1268c13c3975fb5ad24ff3363078ff15b5d7a6a7
oweitman <oweitman@gmx.de>
2025-07-13 21:04:17 +0200

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,33 +4,32 @@ import config from '@iobroker/eslint-config';
 
 export default [
     ...config,
-
     {
         // specify files to exclude from linting here
         ignores: [
-            'widgets/skiinfo/dist/skiinfo-dist.js',
-            '.dev-server/**',
-            'backup/**',
+            '.dev-server/',
+            '.vscode/',
             '*.test.js',
             'test/**/*.js',
             '*.config.mjs',
             'build',
+            'dist',
             'admin/build',
             'admin/words.js',
             'admin/admin.d.ts',
-            '**/adapter-config.d.ts'
-        ]
+            'admin/blockly.js',
+            '**/adapter-config.d.ts',
+        ],
     },
-
     {
         // you may disable some 'jsdoc' warnings - but using jsdoc is highly recommended
         // as this improves maintainability. jsdoc warnings will not block buiuld process.
         rules: {
-            "@typescript-eslint/no-unused-vars": "warn",
-            "prettier/prettier": "warn",
-            "no-debugger": "warn",
-            'jsdoc/require-jsdoc': 'warn',
+            // 'jsdoc/require-jsdoc': 'off',
+            // 'jsdoc/require-param': 'off',
+            // 'jsdoc/require-param-description': 'off',
+            // 'jsdoc/require-returns-description': 'off',
+            // 'jsdoc/require-returns-check': 'off',
         },
     },
-
 ];
